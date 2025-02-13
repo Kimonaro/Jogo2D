cfdc6b6f33ab75ebb69d6da37a07a1c43dbed38f
Kimonaro <g.kira@escolar.ifrn.edu.br>
2025-02-13 17:45:33 -0300

    inimigo

---
+++ 2024.12.10/Assets/Script/Bad.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bad : MonoBehaviour
+{
+    public int fast = 5;
+
+    public int acula = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void RebolarMato()
+    {
+        if (acula == 1)
+        {
+            acula = 2;
+        }
+        else if (acula == 2)
+        {
+            acula = 1;
+        } 
+            
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Muro")
+        {
+            RebolarMato();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (acula == 1)
+        {
+            gameObject.transform.position += new Vector3(fast * Time.deltaTime, 0, 0);
+        }
+        else if (acula == 2)
+        {
+            gameObject.transform.position += new Vector3(-fast * Time.deltaTime, 0, 0);
+        }
+    }
+}
